912c0b77d9fb66329f86b0f16fc437e3cfd2997a
Willem Poortman <wp@hyva.io>
2022-05-05 13:48:07 +0200

    [main]:+ Added the document event magewire:available on Livewire load (via RequireJS)

--- src/view/base/web/js/magewire-init.js
+++ src/view/base/web/js/magewire-init.js
@@ -1,17 +1,22 @@
 define([
+    'require',
     'magewire',
     'magewireEvent'
-], function (mw, mwe) {
+], function (requirejs, mw, mwe) {
     'use strict';
 
-    // Bind 'magewire' events onto it 'livewire' predecessor
-    const magewireEvents = ['load', 'available', 'update'].forEach((target, index) => {
+    // Bind 'magewire' events onto it 'livewire' predecessor.
+    const magewireEvents = ['load', 'update'].forEach((target, index) => {
         document.addEventListener('livewire:' + target, () => {
             mwe.dispatchMagewireEvent(target)
         })
     })
 
     if (window.magewire) {
+        if (requirejs.defined('Livewire')) {
+            mwe.dispatchMagewireEvent('available')
+        }
+
         window.magewire.start();
     }
 });
